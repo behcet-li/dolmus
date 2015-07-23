4d7b86340f1429769ad903e132c67d9a771406db
Behcet <behcet@dolm.us>
2015-07-22 22:54:08 -0400

    Image optimization. Grunt to rescue

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,74 +1,71 @@
-module.exports = function(grunt) {
+'use strict';
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            main: {
-                src: 'js/<%= pkg.name %>.js',
-                dest: 'js/<%= pkg.name %>.min.js'
-            }
+module.exports = function (grunt) {
+  grunt.initConfig({
+    imagemin: {
+      png: {
+        options: {
+          optimizationLevel: 7
         },
-        less: {
-            expanded: {
-                options: {
-                    paths: ["css"]
-                },
-                files: {
-                    "css/<%= pkg.name %>.css": "less/<%= pkg.name %>.less"
-                }
-            },
-            minified: {
-                options: {
-                    paths: ["css"],
-                    cleancss: true
-                },
-                files: {
-                    "css/<%= pkg.name %>.min.css": "less/<%= pkg.name %>.less"
-                }
-            }
+        files: [{
+          // Set to true to enable the following options…
+          expand: true,
+          cwd: '_site/img/',
+          src: ['**/*.png'],
+          dest: '_site/img/',
+          ext: '.png'
+        }]
+      },
+      jpg: {
+        options: {
+          progressive: true
         },
-        banner: '/*!\n' +
-            ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-            ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-            ' * Licensed under <%= pkg.license %> (https://spdx.org/licenses/<%= pkg.license %>)\n' +
-            ' */\n',
-        usebanner: {
-            dist: {
-                options: {
-                    position: 'top',
-                    banner: '<%= banner %>'
-                },
-                files: {
-                    src: ['css/<%= pkg.name %>.css', 'css/<%= pkg.name %>.min.css', 'js/<%= pkg.name %>.min.js']
-                }
-            }
+        files: [{
+          // Set to true to enable the following options…
+          expand: true,
+          cwd: '_site/',
+          src: ['**/*.jpg'],
+          dest: '_site/',
+          ext: '.jpg'
+        }]
+      },
+    },
+    banner: '/*!\n' +
+    ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+    ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+    ' * Licensed under <%= pkg.license %> (https://spdx.org/licenses/<%= pkg.license %>)\n' +
+    ' */\n',
+    usebanner: {
+      dist: {
+        options: {
+          position: 'top',
+          banner: '<%= banner %>'
         },
-        watch: {
-            scripts: {
-                files: ['js/<%= pkg.name %>.js'],
-                tasks: ['uglify'],
-                options: {
-                    spawn: false,
-                },
-            },
-            less: {
-                files: ['less/*.less'],
-                tasks: ['less'],
-                options: {
-                    spawn: false,
-                }
-            },
+        files: {
+          src: ['css/<%= pkg.name %>.css', 'css/<%= pkg.name %>.min.css', 'js/<%= pkg.name %>.min.js']
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['js/<%= pkg.name %>.js'],
+        tasks: ['uglify'],
+        options: {
+          spawn: false,
         },
-    });
+      },
+      less: {
+        files: ['less/*.less'],
+        tasks: ['less'],
+        options: {
+          spawn: false,
+        }
+      },
+    },
+  });
 
-    // Load the plugins.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-banner');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-    // Default task(s).
-    grunt.registerTask('default', ['uglify', 'less', 'usebanner']);
-
+  grunt.registerTask('img', ['imagemin']);
+  grunt.registerTask('default', ['img']);
 };
